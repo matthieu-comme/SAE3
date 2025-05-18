116091abe525f85a829403f0393e2da5812c4403
Matthieu C. <comme.matthieu@gmail.com>
2025-05-18 15:52:32 +0200

    Delete SQL directory

--- SQL/table_user.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE utilisateur ( 
-pseudo VARCHAR(20),
-mdp VARCHAR(50),
-CONSTRAINT pk_user PRIMARY KEY(pseudo));
-
-INSERT INTO utilisateur VALUES('lutin1','mdp1');
-INSERT INTO utilisateur VALUES('lutin2','mdp2');
-INSERT INTO utilisateur VALUES('LUTIN1','MDP1');	
