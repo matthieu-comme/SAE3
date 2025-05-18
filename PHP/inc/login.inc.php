1b56127bc8cd5fd0bd61faa509754c3e3e7d8815
Matthieu C. <comme.matthieu@gmail.com>
2025-05-18 15:52:09 +0200

    Delete PHP directory

--- PHP/inc/login.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (isset($_POST['deco'])) { // si le bouton "déconnecter" a été cliqué
-	session_unset();
-	session_destroy();
-}
-if (!isset($_SESSION['pseudo'])) // si non connecté, redirection auto
-	header('Location: login.php');
